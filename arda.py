fef231f7dd18503e95d77b3e4db0f5d1f5f11ca6
ardapermit <arda.qway@gmail.com>
2025-05-18 17:53:42 +0300

    ardapermit02

---
+++ arda.py
@@ -0,0 +1,38 @@
+print("\033(1:32,40M")
+# print(" ╗"+"=""20+"g")
+print("╔═══════════════════════════════════════╗")
+# print("║\033(1;31;40m    VEKTOREL APP  \033[1;32;40m  ║")
+print("║     ║ ")                              ║         
+print("║     ║ ")                              ║
+print("║     ║ ")                              ║
+print("║     ║ ")                              ║
+print("║     ║ ")                              ║
+print("╚═══════════════════════════════════════╝")
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Vektörel bilişim","Python98\ işareti ile alt satırdan devam edilebilir
+grubu","print dersi",sep="--" ,end="***")
+print ("Günün konusu print'te parametreler.")
+
